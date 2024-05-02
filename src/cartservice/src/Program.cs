9240cc37846dc5df3fbb76dc304d3611b26a6403
Shailendra Singh <b4shailen@gmail.com>
2024-05-03 00:02:30 +0530

    Update Program.cs for CI-11 test

--- src/cartservice/src/Program.cs
+++ src/cartservice/src/Program.cs
@@ -16,6 +16,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using cartservice;
 
+
 CreateHostBuilder(args).Build().Run();
 
 static IHostBuilder CreateHostBuilder(string[] args) =>
