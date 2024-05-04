7d308b7a01c87636eeeab49f7196d1fc91cdbbe7
Shailendra Singh <b4shailen@gmail.com>
2024-05-04 22:16:40 +0530

    Update AdService.java - CI test

--- src/adservice/src/main/java/hipstershop/AdService.java
+++ src/adservice/src/main/java/hipstershop/AdService.java
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * Adding this line to introduce change in code to test the CI-demo process only. 
+ * Adding this line to introduce a change. 
  */
 
 package hipstershop;
